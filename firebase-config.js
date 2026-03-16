b61cf3cf0dc3c6b3da1b59bf21db30174bd8fb61
Antoine Dhayer <antoinedhayer@Ordinateur-portable-de-Antoine.local>
2026-03-16 20:41:44 +0100

    Fix: Sync Firebase config syntax with project infrastructure

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,20 @@
-// REMPLACEZ LES VALEURS CI-DESSOUS PAR VOTRE CONFIGURATION FIREBASE
-// Vous les trouverez dans les paramètres de votre projet sur https://console.firebase.google.com/
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_AUTH_DOMAIN",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_STORAGE_BUCKET",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyAOb6naVQ8GIN4irJuT4VuAK3x-S7xSHtk",
+  authDomain: "krew-9d6e4.firebaseapp.com",
+  projectId: "krew-9d6e4",
+  storageBucket: "krew-9d6e4.firebasestorage.app",
+  messagingSenderId: "407652934183",
+  appId: "1:407652934183:web:d9825c9334bd1453286402",
+  measurementId: "G-LM0HED743L"
 };
 
-// Initialisation de Firebase
+// Initialisation de Firebase (Mode Compatibilité)
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
